798ed67de0457af01ee6d4544930aba25d3617f4
ubuntu_sbz2 <1700013038@pku.edu.cn>
2022-10-27 23:02:20 -0700

    second push from ubuntu_sbz

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+
+#include "locker.h"
+#include "threadpool.h"
+#include "http_conn.h"
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
